00a33960cb484baf2b216e73c260eac9d08e52e7
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-02-03 23:14:37 +0200

    Вирішує завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector("body")
+//  console.log(btnStart)
+//  console.log(btnStop)
+// console.log(bodyColor)
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+let intervalId = null;
+
+btnStart.addEventListener("click", startColorChange);
+btnStop.addEventListener("click", stopColorChange);
+
+function startColorChange (){
+    intervalId = setInterval(onChangeColorButtonClick, 1000);
+    btnStart.toggleAttribute('disabled');
+}
+
+function onChangeColorButtonClick () {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+}
+
+function stopColorChange (){
+    clearInterval(intervalId);
+    btnStart.removeAttribute('disabled');
+}
